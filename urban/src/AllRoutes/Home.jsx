1ebc9ebdb54fec70197728b01dd09e0d5600fdc6
Vaibhav <vaibhav.2071998@gmail.com>
2022-11-12 01:28:52 +0530

    Added day4 work

--- urban/src/AllRoutes/Home.jsx
+++ urban/src/AllRoutes/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Home_Container from "../Components/Home_Container";
 
-
 const Home = () => {
   return (
     <div>
---
+++ urban/src/Components/Footer.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { Box, HStack, Text, Stack, Heading, Image } from "@chakra-ui/react";
+import {
+  BsFacebook,
+  BsTwitter,
+  BsInstagram,
+  BsYoutube,
+  BsLinkedin,
+  BsPinterest,
+} from "react-icons/bs";
+
+const Footer = () => {
+  return (
+    <div>
+      <Box w={"100%"} h={"500px"} bg={"black"} mt={20} color={"white"}>
+        <Box
+          w={"90%"}
+          h={"400px"}
+          //   border="1px solid white"
+          m={"auto"}
+          pt={"10px"}
+        >
+          <Box py={5} borderBottom="1px">
+            <HStack gap={5}>
+              <Text>About Us</Text>
+              <Text>IC Impact</Text>
+              <Text>Trems & Conditions</Text>
+              <Text>Privacy Policy</Text>
+              <Text>Intrest Base Advertising</Text>
+              <Text>Anti Discrimination Policy</Text>
+              <Text>Patner Welfare Policy</Text>
+            </HStack>
+            <HStack gap={5} mt={5}>
+              <Text>Information Security Policy Statement & Objective</Text>
+              <Text>Blog</Text>
+              <Text>Reviews</Text>
+              <Text>Near me</Text>
+              <Text>Careers</Text>
+              <Text>Gift Cards</Text>
+              <Text>Contact Us</Text>
+              <Text>Quick Links</Text>
+            </HStack>
+          </Box>
+          <HStack>
+            <Heading as="h5" size="sm" mt={5}>
+              Serving In
+            </Heading>
+          </HStack>
+          <HStack>
+            <Heading as="h6" size="xs" mt={5}>
+              USA
+            </Heading>
+          </HStack>
+          <HStack gap={5} mt={5}>
+            <Text>Austin</Text>
+            <Text>NewYork City</Text>
+          </HStack>
+          <HStack>
+            <Heading as={"h6"} size="sm" mt={5}>
+              India
+            </Heading>
+          </HStack>
+          <HStack gap={5} mt={5}>
+            <Text>Mumbai</Text>
+            <Text>Delhi</Text>
+            <Text>Surat</Text>
+            <Text>Pune</Text>
+            <Text>Nagpure</Text>
+            <Text>Hydrabad</Text>
+            <Text>Bangalore</Text>
+            <Text>Patna</Text>
+          </HStack>
+        </Box>
+      </Box>
+
+      {/* footer lower part */}
+      <Box w={"100%"} h={"500px"} bg={"black"} color={"white"}>
+        <HStack>
+          {/* social icons */}
+          <HStack>
+            <BsFacebook size={"1.5em"} />
+            <BsTwitter size={"1.5em"} />
+            <BsInstagram size={"1.5em"} />
+            <BsYoutube size={"1.5em"} />
+            <BsLinkedin size={"1.5em"} />
+            <BsPinterest size={"1.5em"} />
+          </HStack>
+
+          <HStack>
+            <Image
+              src="https://images.urbanclap.com/image/upload/categories/category_v2/category_7f907eb0.png"
+              alt="playStore"
+            />
+          </HStack>
+        </HStack>
+      </Box>
+    </div>
+  );
+};
+
+export default Footer;
